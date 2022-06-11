c4da370e1d14f89d7ce6429e017b7e9287e31015
Joanna Kang <jfrences11@gmail.com>
2022-06-11 22:18:25 +0100

    fix: remove duplication from category response

--- src/controller/category.js
+++ src/controller/category.js
@@ -2,10 +2,23 @@ import { Category } from '../model/category.js';
 
 export async function createCategory(req, res) {
   try {
-    const { categoryName } = req.body;
-    const newCategory = await Category.create({
-      categoryName,
-    });
+    const categories = await Category.findAll();
+    let newCategory;
+    // create base category set
+    if (categories.length === 0) {
+      const BASE_CATEGORY = ['Paper', 'Glass', 'Plastic', 'Metal'];
+      for (let i = 0; i < BASE_CATEGORY.length; i++) {
+        newCategory = await Category.create({
+          categoryName: BASE_CATEGORY[i],
+        });
+      }
+    } else {
+      // add additional category if it's needed
+      const { categoryName } = req.body;
+      newCategory = await Category.create({
+        categoryName,
+      });
+    }
     res
       .status(200)
       .json({ newCategory, alert: 'Successfully saved a new category!' });
@@ -16,9 +29,11 @@ export async function createCategory(req, res) {
 
 export async function getCategories(req, res) {
   try {
-    const categories = await Category.findAll();
-    res.status(200).json(categories);
+    const categories = await Category.findAll({ raw: true });
+    const categoriesArray = categories.map((category) => category.categoryName);
+    const removeDuplication = [...new Set(categoriesArray)];
+    res.status(200).json(removeDuplication);
   } catch (error) {
-    res.status(401).json(categories);
+    res.status(401).json(error);
   }
 }
